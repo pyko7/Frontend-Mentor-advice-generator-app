2fcabf2e7e4faf0271e60a29dd0b5a5212176d5e
Julien <julien7henry@gmail.com>
2022-02-19 15:21:40 +0100

    adding of js file

---
+++ js/script.js
@@ -0,0 +1,31 @@
+//function gets data from api
+const getAdvice = async () =>{
+    let res = await fetch('https://api.adviceslip.com/advice');
+    let data = await res.json();
+    if(res.ok){
+        return data;
+    }else{
+        console.log("error");
+    }
+}
+
+//function displays the data on the screen
+const displayAdvice = async () =>{
+    let newAdvice = await getAdvice();
+    const adviceNumber = document.getElementById('adviceNumber');
+    const advice = document.getElementById('advice');
+
+    adviceNumber.textContent= newAdvice.slip.id;
+    advice.textContent= newAdvice.slip.advice;
+}
+
+//function refreshes and shows new advice on screen
+const refreshAdvice = () =>{
+    const refreshBtn = document.getElementById('refreshBtn');
+    refreshBtn.addEventListener('click', ()=>{
+        displayAdvice();
+    } )
+}
+
+displayAdvice();
+refreshAdvice();
